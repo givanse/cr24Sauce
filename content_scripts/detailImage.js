c2f1cf75f2686aee1fba30c482d505c2f70e501a
givanse <silvag777@gmail.com>
2013-08-08 12:52:37 -0500

    workaround the 'CSS not applied' problem

--- content_scripts/detailImage.js
+++ content_scripts/detailImage.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function() {
-    console.log('detailing image');
+    
+    /* This shouldn't be needed. */
+    var style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    style.href = chrome.extension.getURL('style.css');
+    (document.head||document.documentElement).appendChild(style);
+    /* This shouldn't be needed. The manifest should take care... */
+
     var url = $('img').attr('src');
     var details = $('<div/>').attr('id', 'details')
                              .html("File: " + url);
